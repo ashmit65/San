bf0b1757a10e3167fe7ae5a1a2ab84666bdff8d9
ashmit65 <146719150+ashmit65@users.noreply.github.com>
2025-05-16 21:07:19 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   basePath: '/San', // Replace with your actual repository name
   assetPrefix: '/San',
 };
